facd1ca931364eb7c383c08fd4dd19b22e51a90a
Juldas RAMDE <juldas1.ramde@orange.com>
2026-05-20 08:37:06 +0000

    feat!: add about page

--- src/app/layouts/layout.ts
+++ src/app/layouts/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { Hero } from './hero/hero';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, About],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
